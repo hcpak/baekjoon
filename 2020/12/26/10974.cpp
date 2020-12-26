80e4b48c10ad58cf3b7741b9d2d1b53496a7a186
hyungchul pak <jasotn12@naver.com>
2020-12-26 20:01:01 +0900

    12월 26일 알고리즘

---
+++ 2020/12/26/10974.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <stack>
+using namespace std;
+
+void dfs(int number){
+    bool visited[8] = {};
+    visited[number] = true;
+    stack<int> s;
+    s.push(number);
+    while(!s.empty()){
+        int 
+    }
+}
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    
+    return 0;
+}
